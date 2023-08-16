5534d234d44cbbe93db5fb4b560a7cdec9f6f2b9
SchuleR <i@iprub.ru>
2023-08-16 17:14:07 +0300

    Добавил в репозиторий

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+
+Console.WriteLine("Ваше число:"); 
+string a = Console.ReadLine();
+Console.WriteLine(a);
